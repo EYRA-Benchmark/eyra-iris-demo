a8ef30158f9c97a6fbc9701f21189f41000e74da
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-08-21 13:45:38 +0200

    Remove training code from submission container

--- model_creation/train.py
+++ model_creation/train.py
@@ -31,16 +31,18 @@ def save_classifier(clf, path):
 
 
 if __name__ == "__main__":
-    # Run the algorithm on your local copy of the data by typing:
-    # python algorithm_scr/train.py
+    # Create the model using a local copy of the data by typing:
+    # python model_creation/train.py
+    root = Path(__file__).absolute().parent.parent
+    submission_dir = 'demo_submission_5f2d4700-b379-11e9-af77-acde48001122'
+
     try:
-        os.makedirs(Path('algorithm_src')/'model')
+        os.makedirs(Path(root)/submission_dir/'model')
     except FileExistsError:
         pass
 
-    train_file = Path('data')/'input'/'iris_train.csv'
-    test_file = Path('data')/'input'/'iris_public_test_data.csv'
-    out_file = Path('algorithm_src')/'model'/'iris_svm_model'
+    train_file = Path(root)/'data'/'iris_train.csv'
+    out_file=Path(root)/submission_dir/'model'/'iris_svm_model'
 
     clf = train_svm(train_file)
     save_classifier(clf, out_file)
